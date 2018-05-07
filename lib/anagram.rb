23e550549d3c88d7d403cd73fa9318d7db0ce722
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-07 19:17:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,3 +18,20 @@ class Anagram
   end
   #binding.pry
 end
+
+=begin
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
+=end
+
